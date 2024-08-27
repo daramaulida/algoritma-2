bd7bcb040939b481bd7dbbd72010975d1e43d9de
daramaulida <daraksp291@gmail.com>
2024-08-27 11:30:55 +0700

    membuat steatmen dan ekspresi

---
+++ 4 html dalam php & membuat steatmen dan ekspresi.php
@@ -0,0 +1,9 @@
+<?php
+echo "ini statement 1<p>";
+echo "ini statement 2<p>";
+$b =7;
+$c =3;
+$a = $b * $c;
+echo "<p>ini ekspresi -> $a = $b * $c <p> ";
+echo "<p>yaitu : ".$a;
+echo "<p>Nama:Dara Maulida";
