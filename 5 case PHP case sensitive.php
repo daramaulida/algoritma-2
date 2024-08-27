8e9b85d3642ceeb9df8948a8afd50a0ac7a9a89b
daramaulida <daraksp291@gmail.com>
2024-08-27 11:54:59 +0700

    case sensitive

---
+++ 5 case PHP case sensitive.php
@@ -0,0 +1,32 @@
+<?php
+
+$nama = "daramaulida";
+$NAMA = "dara";
+$umur = "16";
+$tempatlahir = "K.Pakel";
+$hobi = "nonton drakor";
+
+const INIKONSTANTA =123;
+$ini_variabel = 30;
+$iniJugaVariabel = 55;
+$iniJugaVariabel;
+
+echo "<p>Nama Saya Adalah $nama";
+echo "<p>Nama Panggilan Saya Adalah $NAMA";
+echo "<p>Umur Saya Adalah $umur";
+echo "<p>Tempat Lahir Saya $tempatlahir";
+echo "<p>Hobi Saya Adalah $hobi";
+
+
+
+echo
+     
+"<p><strong><br>coba pakai blok program",
+"<p>Nama Saya Adalah $nama",
+"<p>Nama Panggilan Saya Adalah $NAMA",
+"<p>Umur Saya Adalah $umur",
+"<p>Tempat Lahir Saya $tempatlahir",
+"<p>Hobi Saya Adalah $hobi";
+"<p>Nama:Dara Maulida";
+
+?>
