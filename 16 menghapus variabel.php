b0290e401eb2fd763e1f0ab4211e1ccc95475ee0
daramaulida <daraksp291@gmail.com>
2024-08-28 13:26:13 +0700

    menghapus variabel

---
+++ 16 menghapus variabel.php
@@ -0,0 +1,13 @@
+
+<?php
+
+// membuat variabel $tmp
+$tmp = 2008;
+
+// menghapus variabel $tmp
+//unset($tmp);
+
+// mencoba mengakses variabel $tmp
+echo "<h1>". $tmp;
+echo "<br>Nama:Dara Maulida";
+
