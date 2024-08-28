e4183f3d5488c7713fa3b81140cae7f826caf457
daramaulida <daraksp291@gmail.com>
2024-08-28 11:57:24 +0700

    tipe data pada php

---
+++ 13 tipe data pada php.php
@@ -0,0 +1,42 @@
+<?php
+
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "Dara Mulida";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 45;
+
+// tipe data float
+$tinggi = 145;
+
+// tipe data boolean
+$menikah = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $menikah";
+echo "<p>";
+
+if ($menikah1=true) {
+    echo "jangan menikah karena menikah gaenak";
+} else {
+    echo "jangan menikah dulu sebelum sukses";
+}
+echo "<p>";
+
+if ($menikah2=false) {
+    echo "jangan menikah karena menikah gaenak";
+} else {
+    echo "jangan menikah dulu sebelum sukses";
+}
+echo "<br><strong>Nama:Dara Maulida";
