8fbca0a06cdb9b82e4900fec7f713084435183d9
daramaulida <daraksp291@gmail.com>
2024-08-28 11:36:48 +0700

    mencetak dengan printf

---
+++ 12 mencetak dengan printf.php
@@ -0,0 +1,20 @@
+<?php
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 100000;
+
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga";
+
+// jika kita cetak dengan print:
+print "<p></p>Harganya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("<p>Harganya adalah Rp %.2f", $harga);
+
+// jika kita cetak dengan printf
+printf("<p>Harganya adalah Rp %.3f", $harga);
+
+// jika kita cetak dengan printf
+printf("<p>Harganya adalah Rp %.4f", $harga);
+
+echo "<p><strong>Nama:Dara Maulida";
